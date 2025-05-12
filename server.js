ae3cc9282f901b5d6811b38d214a54b2f669b857
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-05-12 01:17:28 -0700

    Register Mongoose models for User, Role, UserRole, Permission, RolePermission, Course, Batch, BatchCourse, Group, Phase, Week, BatchUser, GroupUser, CourseInstructor, BatchInstructor, LiveSession, GroupSession, Attendance, and Enrollment

--- server.js
+++ server.js
@@ -9,6 +9,27 @@ dotenv.config();
 // Connect to database
 connectDB();
 
+// Import models (this ensures they're registered with Mongoose)
+require('./models/User');
+require('./models/Role');
+require('./models/UserRole');
+require('./models/Permission');
+require('./models/RolePermission');
+require('./models/Course');
+require('./models/Batch');
+require('./models/BatchCourse');
+require('./models/Group');
+require('./models/Phase');
+require('./models/Week');
+require('./models/BatchUser');
+require('./models/GroupUser');
+require('./models/CourseInstructor');
+require('./models/BatchInstructor');
+require('./models/LiveSession');
+require('./models/GroupSession');
+require('./models/Attendance');
+require('./models/Enrollment');
+
 // Initialize express
 const app = express();
 
