eaa76402ad5e3149f1e6276b25c64f2aafbedffb
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-06-04 23:51:55 -0700

    Add phaseRoutes to the server configuration, integrating phase management into the API for enhanced functionality.

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const authRoutes = require('./auth');
 const userRoutes = require('./users');
 const courseRoutes = require('./courses');
 const batchRoutes = require('./batches');
+const phaseRoutes = require('./phases');
 
 module.exports = {
   authRoutes,
   userRoutes,
   courseRoutes,
-  batchRoutes
+  batchRoutes,
+  phaseRoutes
 };
