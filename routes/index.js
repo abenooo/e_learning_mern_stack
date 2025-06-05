0dee87b6fb540b7f1a3f7bee00dc80b80b1d0f4c
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-06-05 04:12:05 -0700

    Integrate group routes into server configuration, enabling access to group-related endpoints. This update enhances the API's functionality for managing groups.

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const userRoutes = require('./users');
 const courseRoutes = require('./courses');
 const batchRoutes = require('./batches');
 const phaseRoutes = require('./phases');
-
+const groupRoutes = require('./groups');
+ 
 module.exports = {
   authRoutes,
   userRoutes,
   courseRoutes,
   batchRoutes,
-  phaseRoutes
+  phaseRoutes,
+  groupRoutes
 };
