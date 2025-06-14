580631fb46f152f2839f9290b74438fc9d4183aa
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-06-14 05:12:23 -0700

    Add CRUD operations for group sessions in the API, including routes for retrieving, creating, updating, and deleting individual sessions.

--- routes/groupSessions.js
+++ routes/groupSessions.js
@@ -1,8 +1,26 @@
 const express = require('express');
-const { getGroupSessions } = require('../controllers/groupSessions');
+const { 
+  getGroupSessions,
+  getGroupSession,
+  createGroupSession,
+  updateGroupSession,
+  deleteGroupSession
+} = require('../controllers/groupSessions');
 const router = express.Router();
 
 // Get all group sessions
 router.get('/', getGroupSessions);
+
+// Get single group session
+router.get('/:id', getGroupSession);
+
+// Create new group session
+router.post('/', createGroupSession);
+
+// Update group session
+router.put('/:id', updateGroupSession);
+
+// Delete group session
+router.delete('/:id', deleteGroupSession);
 
 module.exports = router; 
