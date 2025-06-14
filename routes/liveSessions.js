1746898de66bd0f505bc3e583679e5831fa9b17a
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-06-14 04:05:49 -0700

    Add CRUD operations for live sessions in the API, including routes for retrieving, creating, updating, and deleting individual sessions.

--- routes/liveSessions.js
+++ routes/liveSessions.js
@@ -1,8 +1,26 @@
 const express = require('express');
-const { getLiveSessions } = require('../controllers/liveSessions');
+const { 
+  getLiveSessions,
+  getLiveSession,
+  createLiveSession,
+  updateLiveSession,
+  deleteLiveSession
+} = require('../controllers/liveSessions');
 const router = express.Router();
 
 // Get all live sessions
 router.get('/', getLiveSessions);
+
+// Get single live session
+router.get('/:id', getLiveSession);
+
+// Create new live session
+router.post('/', createLiveSession);
+
+// Update live session
+router.put('/:id', updateLiveSession);
+
+// Delete live session
+router.delete('/:id', deleteLiveSession);
 
 module.exports = router; 
