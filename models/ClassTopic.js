0d9ea5d10ba270698566a93473a4c99b19582575
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-06-18 09:00:35 -0700

    Add ClassTopic model to define schema for class topics, including fields for week relationships, title, order, and associated class components. This enhances the data structure for class topics in the application.

---
+++ models/ClassTopic.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ClassTopicSchema = new Schema({
+  week_id: { type: Schema.Types.ObjectId, ref: 'Week', required: true },
+  title: { type: String, required: true },
+  order: { type: Number, required: true },
+  hash: { type: String },
+  description: { type: String },
+  has_checklist: { type: Boolean, default: false },
+  class_components: [{ type: Schema.Types.ObjectId, ref: 'ClassComponent' }],
+  class_video_section_by_sections: [{ type: Schema.Types.ObjectId, ref: 'ClassVideoSectionBySection' }],
+  class_video_live_sessions: [{ type: Schema.Types.ObjectId, ref: 'ClassVideoLiveSession' }]
+});
+
+module.exports = mongoose.model('ClassTopic', ClassTopicSchema);
